d4babe2c955bf98e102da6f88b29bcf6b240f5f9
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-21 18:34:40 -0400

    Display contacts on click

--- scripts.js
+++ scripts.js
@@ -4,12 +4,13 @@ let displayContactBtn = document.querySelector('#display-btn');
 let emailInput = document.querySelector('#email');
 let nameInput = document.querySelector('#name');
 let form = document.querySelector('form');
+let displayArea = document.querySelector('.display-area');
 
 // Event listeners
 addContactBtn.addEventListener('click', storeFormData);
+displayContactBtn.addEventListener('click', displayContacts);
 
 // Functions
-
 function storeFormData(){
   if (isEmpty()) { createContactsObj(); }
   updateContactsObj(nameInput.value, emailInput.value);
@@ -25,10 +26,28 @@ function createContactsObj() {
 }
 
 function updateContactsObj(name, email) {
-  let contacts = localStorage.getItem('contacts');
-  contacts = JSON.parse(contacts);
+  contacts = retreiveContactsObj()
+
   contacts[name] = email;
 
   contacts = JSON.stringify(contacts);
   localStorage.setItem('contacts', contacts);
+}
+
+function retreiveContactsObj() {
+  let contacts = localStorage.getItem('contacts');
+  return JSON.parse(contacts);
+}
+
+function displayContacts() {
+  let contacts = retreiveContactsObj();
+  let contactsOL = document.createElement('ol');
+
+  for (let key in contacts ) {
+    let li = document.createElement('li');
+    li.innerText = `${key}: ${contacts[key]}`;
+    contactsOL.appendChild(li);
+  }
+
+  displayArea.appendChild(contactsOL);
 }
