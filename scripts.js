97ee327f956f8f884a0b2de2cacbaeb9226593ac
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-21 16:28:27 -0400

    Create query selectors and click event listener for "add contact" button

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
-/* Use this area for pseudo - coding:
-
-
-
-*/
-
 // Query selectors
-
-
+let addContactBtn = document.querySelector('#register-btn');
+let displayContactBtn = document.querySelector('#display-btn');
+let emailInput = document.querySelector('#email');
+let nameInput = document.querySelector('#name');
 
 // Event listeners
-
+addContactBtn.addEventListener('click', storeFormData);
 
 
 
 // Functions
 
+function storeFormData(){
+  console.log('it works!');
+}
