1d383b5125e339e6ab9c1d6cb79237585eb6982b
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-21 17:44:24 -0400

    Reset form on contact submission

--- scripts.js
+++ scripts.js
@@ -3,20 +3,17 @@ let addContactBtn = document.querySelector('#register-btn');
 let displayContactBtn = document.querySelector('#display-btn');
 let emailInput = document.querySelector('#email');
 let nameInput = document.querySelector('#name');
+let form = document.querySelector('form');
 
 // Event listeners
 addContactBtn.addEventListener('click', storeFormData);
 
-
 // Functions
 
 function storeFormData(){
-  let name = nameInput.value;
-  let email = emailInput.value;
-  
-  if (isEmpty()) createContactsObj();
-
-  updateContactsObj(name, email);
+  if (isEmpty()) { createContactsObj(); }
+  updateContactsObj(nameInput.value, emailInput.value);
+  form.reset();
 }
 
 function isEmpty() {
