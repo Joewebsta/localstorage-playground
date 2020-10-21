691e51196fee46f848fa5a633a3e3fcea8ad4c25
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-21 17:39:11 -0400

    Refactor storeFormData function

--- scripts.js
+++ scripts.js
@@ -11,19 +11,27 @@ addContactBtn.addEventListener('click', storeFormData);
 // Functions
 
 function storeFormData(){
-  let email = emailInput.value;
   let name = nameInput.value;
+  let email = emailInput.value;
   
-  if (!localStorage.length) {
-    localStorage.setItem('contacts', JSON.stringify({}));
-  }
+  if (isEmpty()) createContactsObj();
 
+  updateContactsObj(name, email);
+}
+
+function isEmpty() {
+  return localStorage.length === 0;
+}
+
+function createContactsObj() {
+  localStorage.setItem('contacts', JSON.stringify({}));
+}
+
+function updateContactsObj(name, email) {
   let contacts = localStorage.getItem('contacts');
   contacts = JSON.parse(contacts);
   contacts[name] = email;
 
   contacts = JSON.stringify(contacts);
-  localStorage.setItem('contacts', contacts)
-
-  console.log(localStorage);
+  localStorage.setItem('contacts', contacts);
 }
